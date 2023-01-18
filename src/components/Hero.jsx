12203973b7f543a4d05686345b62275b6616b635
BeastPleasure <beastpleasure@gmail.com>
2023-01-18 13:22:40 -0500

    xsw

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,37 @@ const Hero = () => {
 
   return (
 
-    <div>Hero</div>
+    <div className='w-full h-[90vh]'>
+
+        <img 
+            src='https://images.unsplash.com/photo-1538964173425-93884d739596?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80' 
+            alt='/'
+            className='w-full h-full object-cover'
+        />
+
+        <div className='max-w-[1140px] m-auto'>
+
+            <div className='absolute top-[40%] w-full md:-[50%] max-w-[600px] h-full flex flex-col text-white p-4'>
+
+                <h1 className='font-bold text-4xl text-[#ff0000]'> Find Your Special Trip </h1>
+
+                <h2 className='text-4xl py-4 italic text-[#ff0000]'> With Weekway </h2>
+
+                <p className='text-[#ff9900]'> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+                    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum 
+                </p>
+
+            </div>
+
+        </div>
+
+
+    </div>
 
   );
 
