85d70c5d1cd12836e3d79267ece0778db0655650
BeastPleasure <beastpleasure@gmail.com>
2023-01-18 10:03:53 -0500

    zxc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {AiFillPhone, AiOutlineClockCircle} from 'react-icons/ai';
+import {BsChatSquareDots} from 'react-icons/bs';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
 
-export default Header
+  return (
+
+    <div className='flex justify-between'>
+
+        <div> 
+
+            <BsChatSquareDots />
+
+            <h1> WEEKAWAY </h1>
+
+        </div>
+
+        <div> 
+
+            <div>
+
+                <AiOutlineClockCircle />
+
+                <p> 09AM - 05PM </p>
+
+            </div>
+
+            <div>
+
+                <AiFillPhone />
+
+                <p> 1-800-777-1234 </p>
+
+            </div>
+
+            <button> Get A Free Quote </button>
+
+        </div>
+
+    </div>
+
+  );
+
+};
+
+export default Header;
